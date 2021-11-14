e5ba1201f2decea688064368fec8f9e3be45dbeb
lkjhg612 <56185151+lkjhg612@users.noreply.github.com>
2021-11-14 16:03:43 +0700

    Thêm 1 button

--- app/src/main/java/GameBauCua/MainBauCuaActivity.java
+++ app/src/main/java/GameBauCua/MainBauCuaActivity.java
@@ -16,7 +16,7 @@ import java.util.Random;
 
 public class MainBauCuaActivity extends AppCompatActivity {
 
-    Button nutBam, nutThoat;
+    Button nutBam, nutThoat, demo;
     ImageView anh1, anh2, anh3, anh4;
 
     @Override
